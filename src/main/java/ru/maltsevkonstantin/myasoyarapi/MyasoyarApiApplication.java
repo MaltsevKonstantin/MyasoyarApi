3d7c45d482928bc55b83633efb882bfdaabe1253
MaltsevKonstantin <maltsev.konstantin85@yandex.ru>
2022-08-10 22:30:09 +0300

    First

---
+++ src/main/java/ru/maltsevkonstantin/myasoyarapi/MyasoyarApiApplication.java
@@ -0,0 +1,13 @@
+package ru.maltsevkonstantin.myasoyarapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MyasoyarApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MyasoyarApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/ru/maltsevkonstantin/myasoyarapi/MyasoyarApiApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.maltsevkonstantin.myasoyarapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MyasoyarApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
